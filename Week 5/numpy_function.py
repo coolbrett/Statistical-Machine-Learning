d4b7ef027c873ce1c53aa4fac6fcaa1b9fbc9c81
Brett Dale <brettdale828@gmail.com>
2020-09-28 19:30:46 -0400

    updating

--- Week 5/numpy_function.py
+++ Week 5/numpy_function.py
@@ -8,3 +8,12 @@ def sum_of_squares(x_list):
 def sum_of_squares_np(x_list):
     np.square(x_list)
     return np.sum(x_list)
+
+
+def cube_number(num):
+    """
+    This functions returns the parameter cubed
+    :arg: number to be cubed
+    :rtype: int of cubed number
+    """
+    return num * num * num
